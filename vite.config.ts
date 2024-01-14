dd88d6aff994a32808bac883e1ac5b24f69465e0
Hugo Carnicelli <hugo@infogrid.io>
2024-01-14 16:03:02 +0000

    chore: enable local network debugging

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
   plugins: [react(), svgr()],
 });
