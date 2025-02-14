2b39206e481f612ee25dfdc89c0a779c17b3ac94
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-02-14 04:25:18 +0100

    chore: better looking map, fixed loading loop

--- src/components/map-view.tsx
+++ src/components/map-view.tsx
@@ -1,6 +1,6 @@
 import { Box, useTheme } from "@mui/material";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { renderToString } from "react-dom/server";
 import CustomMarkerIcon from "../assets/logo-monogram.svg?react"; // SVG as React component
 
@@ -29,8 +29,8 @@ interface Shop {
 export const fetchShops = async () => {
   const AIRTABLE_API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
   const BASE_ID = "appY0KHiZkP8q5JXL";
-  const TABLE_NAME = "tblFbW11rhW442gdt";
-  const URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+  const TABLE_ID = "tblFbW11rhW442gdt";
+  const URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`;
 
   try {
     const response = await fetch(URL, {
@@ -46,22 +46,24 @@ export const fetchShops = async () => {
 
     const data = await response.json();
 
-    return data.records.map(
-      (record: Record) =>
-        ({
-          id: record.id,
-          name: record.fields.name,
-          address: record.fields.address,
-          lat: record.fields.lat,
-          lon: record.fields.lon,
-          picture: record.fields.picture?.[0]?.thumbnails.large.url,
-          active: record.fields.active,
-          categories: record.fields.categories,
-        } as Shop)
-    );
+    return data.records
+      .map(
+        (record: Record) =>
+          ({
+            id: record.id,
+            name: record.fields.name,
+            address: record.fields.address,
+            lat: record.fields.lat,
+            lon: record.fields.lon,
+            picture: record.fields.picture?.[0]?.thumbnails.large.url,
+            active: record.fields.active,
+            categories: record.fields.categories,
+          } as Shop)
+      )
+      .filter((s: Shop) => s.active) as Shop[];
   } catch (error) {
     console.error("Error fetching data:", error);
-    return [];
+    return [] as Shop[];
   }
 };
 
@@ -69,9 +71,11 @@ const MapView = () => {
   const theme = useTheme();
   // data fetching
   const [shops, setShops] = useState<Shop[]>([]);
+  const hasLoaded = useRef(false);
   useEffect(() => {
-    if (!shops.length) {
+    if (!hasLoaded.current) {
       fetchShops().then(setShops);
+      hasLoaded.current = true;
     }
   }, [shops]);
 
@@ -100,31 +104,32 @@ const MapView = () => {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          width: 16,
-          height: 16,
+          width: 8,
+          height: 8,
           borderRadius: "50%",
           backgroundColor: theme.palette.secondary.main,
           border: `2px solid ${theme.palette.primary.main}`,
-          padding: 8,
+          padding: 4,
         }}
       >
         <CustomMarkerIcon />
       </Box>
     ),
-    iconSize: [24, 24],
-    iconAnchor: [18, 24], // Adjust for alignment
-    popupAnchor: [0, -24],
+    iconSize: [16, 16],
+    iconAnchor: [10, 16], // Adjust for alignment
+    popupAnchor: [0, -16],
   });
 
   return (
+    // TODO: add boundaries (`maxBounds`)
     <MapContainer
       center={[48.8566, 2.3522]}
       zoom={12}
       style={{ height: "100vh", width: "100%" }}
     >
       <TileLayer
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='© <a href="https://carto.com/attributions">CARTO</a>'
       />
       {shops.map((shop) => (
         <Marker key={shop.id} position={[shop.lat, shop.lon]} icon={customIcon}>
