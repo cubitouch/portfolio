ac5f6ad7e40c806e37ce89e1c0081d9415e8b64f
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-02-14 02:10:28 +0100

    chore: add empty new route

---
+++ src/routes/fashion.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import type { Route } from "./+types/fashion";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "Men Sustainable Fashion Shops" }];
+}
+export default function Fashion() {
+  return <Box sx={{}}></Box>;
+}
