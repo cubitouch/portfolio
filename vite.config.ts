d94de835bceded2d402ac73c9520573f0314e0dd
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-02-15 17:52:17 +0100

    chore: embed airtable at build time

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: true,
-    port: 3000,
-  },
-  plugins: [reactRouter(), svgr(), tsconfigPaths()],
+// Load environment variables
+export default defineConfig(({ mode }) => {
+  // Load environment variables from .env (or GitHub Actions secrets)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      host: true,
+      port: 3000,
+    },
+    plugins: [reactRouter(), svgr(), tsconfigPaths()],
+    define: {
+      "process.env.VITE_AIRTABLE_API_KEY": JSON.stringify(
+        env.VITE_AIRTABLE_API_KEY
+      ),
+    },
+  };
 });
