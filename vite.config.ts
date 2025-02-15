ab6dccc682ea65096ce7f8fa2e427b1eeca53827
Hugo Carnicelli <7486151+cubitouch@users.noreply.github.com>
2025-02-15 17:56:19 +0100

    chore: cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { reactRouter } from "@react-router/dev/vite";
 
-// Load environment variables
-export default defineConfig(({ mode }) => {
-  // Load environment variables from .env (or GitHub Actions secrets)
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    server: {
-      host: true,
-      port: 3000,
-    },
-    plugins: [reactRouter(), svgr(), tsconfigPaths()],
-    define: {
-      "process.env.VITE_AIRTABLE_API_KEY": JSON.stringify(
-        env.VITE_AIRTABLE_API_KEY
-      ),
-    },
-  };
+export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
+  plugins: [reactRouter(), svgr(), tsconfigPaths()],
+  define: {
+    "process.env.VITE_AIRTABLE_API_KEY": JSON.stringify(process.env.VITE_AIRTABLE_API_KEY),
+  },
 });
